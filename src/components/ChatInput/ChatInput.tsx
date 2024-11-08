5dc8e33a8b229463c7a9b073da915472612eb82f
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-11-08 06:19:01 +0200

    XAI-11 1. Make ChatInput layout more dense 2. Send query on Enter key 3. Correct common layout and make it more dense 4 Some visual enhancements

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import React from 'react';
 import { useQueryAiHook } from '@/hooks/useQueryAiHook';
-import { Button, TextField } from '@mui/material';
+import { Button, InputAdornment, TextField } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import styles from './chat-input.module.css';
+import { AccountCircle } from '@mui/icons-material';
 
 const ChatInput = () => {
   const { chatAiState, sendAIQuery, setInputValue } = useQueryAiHook();
+
+  const keyPressed = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.keyCode == 13) {
+      sendAIQuery();
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -15,19 +24,31 @@ const ChatInput = () => {
         fullWidth={true}
         value={chatAiState.currentInput}
         margin='dense'
+        size='small'
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
           setInputValue(event.target.value);
         }}
+        onKeyUp={keyPressed}
         disabled={chatAiState.isQuerying}
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position='start' style={{ marginLeft: 0 }}>
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          },
+        }}
       />
       <div className={styles.button}>
         <Button
           variant='contained'
           onClick={sendAIQuery}
           disabled={chatAiState.isQuerying || chatAiState.currentInput.length === 0}
-          style={{ height: '58px' }}
+          style={{ height: '42px', fontSize: '12px' }}
+          endIcon={<SendIcon />}
         >
-          Send query
+          Send
         </Button>
       </div>
     </div>
